9cfe359ca658319de5a7656c683e53e8296683c4
Щиров Игорь <shirov.igor@otr.ru>
2021-03-16 16:28:54 +0300

    add report dto

---
+++ src/main/java/com/innowise/router/dto/Report.java
@@ -0,0 +1,22 @@
+package com.innowise.router.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Report {
+    private String storeName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String saleId;
+    private List<String> customerId;
+    private String tin;
+}
